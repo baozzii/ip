594fa52e40c95fb12559ccdc1e47f5b952517267
Yuhang <161937769+baozzii@users.noreply.github.com>
2025-08-21 19:29:34 +0800

    Level-2

--- src/main/java/Baozii.java
+++ src/main/java/Baozii.java
@@ -1,9 +1,11 @@
 import java.util.*;
+import java.lang.*;
 
 public class Baozii {
     public static void main(String[] args) {
         System.out.println("Hi! I am Baozii. What can I do for you?");
         Scanner scanner = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
 
         while (true) {
             System.out.print(">> ");
@@ -11,8 +13,15 @@ public class Baozii {
             if (msg.equals("bye")) {
                 System.out.println("Bye, have a great day!");
                 break;
+            } else if (msg.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.print(i + 1);
+                    System.out.print(". ");
+                    System.out.println(tasks.get(i));
+                }
             } else {
-                System.out.println(msg);
+                tasks.add(msg);
+                System.out.println("added: " + msg);
             }
         }
     }
