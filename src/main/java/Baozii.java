688b3f7a163cd1f1b7a46e7f5d6032a056b8c1a0
Yuhang <161937769+baozzii@users.noreply.github.com>
2025-08-21 19:09:56 +0800

    Level-0

---
+++ src/main/java/Baozii.java
@@ -0,0 +1,9 @@
+import java.awt.desktop.SystemEventListener;
+
+public class Baozii {
+    public static void main(String[] args) {
+        System.out.println("Hi! I am Baozii. What can I do for you?");
+        System.out.println();
+        System.out.println("Bye, have a great day!");
+    }
+}
