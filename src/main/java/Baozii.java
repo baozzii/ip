ab75977dd4026a3e61686d90dc05cbb990078888
Yuhang <161937769+baozzii@users.noreply.github.com>
2025-08-21 19:13:54 +0800

    Level-1

--- src/main/java/Baozii.java
+++ src/main/java/Baozii.java
@@ -1,9 +1,19 @@
-import java.awt.desktop.SystemEventListener;
+import java.util.*;
 
 public class Baozii {
     public static void main(String[] args) {
         System.out.println("Hi! I am Baozii. What can I do for you?");
-        System.out.println();
-        System.out.println("Bye, have a great day!");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print(">> ");
+            String msg = scanner.next();
+            if (msg.equals("bye")) {
+                System.out.println("Bye, have a great day!");
+                break;
+            } else {
+                System.out.println(msg);
+            }
+        }
     }
 }
