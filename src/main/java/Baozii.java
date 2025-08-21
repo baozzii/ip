fdda1d5fa1fb9f27090775cd020921a401df7b40
Yuhang <161937769+baozzii@users.noreply.github.com>
2025-08-21 19:51:01 +0800

    Added task function

--- src/main/java/Baozii.java
+++ src/main/java/Baozii.java
@@ -5,11 +5,11 @@ public class Baozii {
     public static void main(String[] args) {
         System.out.println("Hi! I am Baozii. What can I do for you?");
         Scanner scanner = new Scanner(System.in);
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         while (true) {
             System.out.print(">> ");
-            String msg = scanner.next();
+            String msg = scanner.nextLine().strip();
             if (msg.equals("bye")) {
                 System.out.println("Bye, have a great day!");
                 break;
@@ -19,9 +19,20 @@ public class Baozii {
                     System.out.print(". ");
                     System.out.println(tasks.get(i));
                 }
+            } else if (msg.startsWith("add")) {
+                msg = msg.substring(3).strip();
+                tasks.add(new Task(msg));
+                System.out.println("added: " + tasks.getLast());
+            } else if (msg.startsWith("mark")) {
+                int i = Integer.parseInt(msg.substring(4).strip());
+                tasks.get(i).mark();
+                System.out.println("marked: " + tasks.get(i));
+            } else if (msg.startsWith("unmark")) {
+                int i = Integer.parseInt(msg.substring(4).strip());
+                tasks.get(i).unmark();
+                System.out.println("unmarked: " + tasks.get(i));
             } else {
-                tasks.add(msg);
-                System.out.println("added: " + msg);
+                System.out.println("Unrecognised keyword");
             }
         }
     }
